8e4fff9265be89c6bfc9dcb27bcbfce5bb62c7c1
Gan Jia Jun <jia_jun_94@hotmail.com>
2023-09-30 17:16:32 +0800

    - Fix syntax error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@ python manage.py migrate
 
 # Please use with care (non-prod)
 # python manage.py flush
-python manage.py seed_database
+python manage.py seed_db
 
 python manage.py runserver 0.0.0.0:8000
