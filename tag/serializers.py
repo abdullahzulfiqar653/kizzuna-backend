2b1a4f9034c036bfd2d065bc3fe1a9f5cbe988ca
Leaw Jia Ning <jianing@jobkred.com>
2023-10-06 12:04:35 +0800

    Allow creating existing tags

--- tag/serializers.py
+++ tag/serializers.py
@@ -5,7 +5,12 @@ from tag.models import Tag
 
 class TagSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True)
+    name = serializers.CharField(max_length=50, validators=[]) # Remove the unique validator
 
     class Meta:
         model = Tag
         fields = ['id', 'name']
+
+    def create(self, validated_data):
+        instance, _ = Tag.objects.get_or_create(**validated_data)
+        return instance
