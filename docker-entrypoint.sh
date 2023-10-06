994136d1415db45871505e1c040260d5f7430902
Leaw Jia Ning <jianing@jobkred.com>
2023-10-06 08:09:25 +0800

    Remove seeding dummy data

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 
-# Please use with care (non-prod)
-# python manage.py flush
-python manage.py seed_db
-
 python manage.py runserver 0.0.0.0:8000
--- workspace/management/commands/seed_db.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from seeders import seed  # Adjust the import based on your project structure
-
-class Command(BaseCommand):
-    help = "Seeds the database with initial fake data"
-
-    def handle(self, *args, **kwargs):
-        seed.run()
-        self.stdout.write(self.style.SUCCESS("Database seeded!"))
