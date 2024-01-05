f2742d5b9f7ad7d1b53f0559c61d6939607154e9
leawning <leawning@users.noreply.github.com>
2024-01-05 21:46:17 +0800

    Handle the case when tag is not added to takeaway (#76)

--- takeaway/signals.py
+++ takeaway/signals.py
@@ -4,9 +4,13 @@ from tag.models import Tag
 def takeaway_tags_changed(sender, action, instance, reverse, pk_set, **kwargs):
     if reverse:
         tag = instance
-    else:  # not reverse
+    elif pk_set:
         tag_id = next(iter(pk_set))
         tag = Tag.objects.get(id=tag_id)
+    else:  # not reverse and len(pk_set) == 0
+        # This happens when tag is already in takeaway.tags
+        # so no tag is added.
+        return
 
     if action in ("post_add", "post_remove"):
         tag.takeaway_count = tag.takeaways.count()
