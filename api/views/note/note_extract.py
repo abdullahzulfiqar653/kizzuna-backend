13505c13e48a5ac338c2b431e87ec1ef0e4e9944
leawning <leawning@users.noreply.github.com>
2024-02-05 10:59:01 +0800

    Fix extract knowledge endpoint (#110)

--- api/views/note/note_extract.py
+++ api/views/note/note_extract.py
@@ -1,5 +1,5 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework import generics
+from rest_framework import generics, response
 
 from api.tasks import analyze_existing_note
 
@@ -9,3 +9,4 @@ class NoteExtractCreateView(generics.CreateAPIView):
     def create(self, request, report_id):
         note = request.note
         analyze_existing_note.delay(note.id)
+        return response.Response({"details": "Job sent"})
