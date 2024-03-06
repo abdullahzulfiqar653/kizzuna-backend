233a3287b6588596a43e5303726bef1ef998e25e
leawning <leawning@users.noreply.github.com>
2024-03-06 12:55:10 +0800

    Fix openai transcriber after upgrading openai package (#134)

--- api/ai/transcribers/openai_transcriber.py
+++ api/ai/transcribers/openai_transcriber.py
@@ -4,6 +4,8 @@ from api.ai.paragrapher import Paragrapher
 
 from .base_transcriber import BaseTranscriber
 
+client = openai.OpenAI()
+
 
 class OpenAITranscriber(BaseTranscriber):
     paragrapher = Paragrapher()
@@ -22,14 +24,14 @@ class OpenAITranscriber(BaseTranscriber):
     def transcribe(self, filepath, filetype, language):
         self.check_filetype(filetype)
         with open(filepath, "rb") as audio_file:
-            transcript = openai.Audio.transcribe(
+            transcript = client.audio.transcriptions.create(
                 model="whisper-1",
                 file=audio_file,
                 response_format="verbose_json",
                 language=language,
             )
-        print("Transcript duration:", transcript["duration"])
-        segments = (segment["text"] for segment in transcript["segments"])
+        print("Transcript duration:", transcript.duration)
+        segments = (segment["text"] for segment in transcript.segments)
         return self.paragrapher.paragraphing(segments)
 
 
