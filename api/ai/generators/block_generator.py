797d7278fe5c84ea038b05054f35afcd869e11fd
leawning <leawning@users.noreply.github.com>
2024-03-25 16:08:13 +0800

    Fix block generator token counting bug (#168)

--- api/ai/generators/block_generator.py
+++ api/ai/generators/block_generator.py
@@ -5,24 +5,29 @@ from langchain_community.chat_models import ChatOpenAI
 from tiktoken import encoding_for_model
 
 from api.ai import config
+from api.ai.generators.utils import token_tracker
 from api.models.block import Block
 from api.models.takeaway import Takeaway
+from api.models.user import User
 
 
 def construct_prompt(question: str, takeaways: QuerySet[Takeaway]):
     encoder = encoding_for_model(config.model)
     prompt_format = gettext("Question: {question}\n\nContext:\n")
     prompt = prompt_format.format(question=question)
-    current_token_count = 0
+    current_token_count = len(encoder.encode(prompt))
     for takeaway in takeaways:
         takeaway_token_count = len(encoder.encode(takeaway.title))
-        if current_token_count + takeaway_token_count > config.chunk_size:
+        if current_token_count + takeaway_token_count + 2 > config.chunk_size:
             return prompt
         prompt += "- " + takeaway.title + "\n"
+        current_token_count += takeaway_token_count + 2
     return prompt
 
 
-def generate_block(block: Block, question: str, takeaways: QuerySet[Takeaway]):
+def generate_block(
+    block: Block, question: str, takeaways: QuerySet[Takeaway], created_by: User
+):
     human_prompt = construct_prompt(question, takeaways)
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -35,6 +40,7 @@ def generate_block(block: Block, question: str, takeaways: QuerySet[Takeaway]):
     )
     llm = ChatOpenAI(model=config.model)
     chain = prompt | llm
-    output = chain.invoke({})
+    with token_tracker(block.asset.project, block, "generate-block", created_by):
+        output = chain.invoke({})
     block.content = {"markdown": output.content}
     block.save()
