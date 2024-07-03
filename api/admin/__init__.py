bf8a094be13f047bc027714643d12c7a9d0546f4
leawning <leawning@users.noreply.github.com>
2024-07-03 22:46:03 +0800

    Allow admin to create free trial subscription for selected workspaces (#249)
    * Allow admin to create free trial subscription for selected workspaces
    
    * Update the workspace admin fields

--- api/admin/__init__.py
+++ api/admin/__init__.py
@@ -3,3 +3,4 @@ from api.admin.product_feature import ProductFeatureAdmin
 from api.admin.stripe_price import StripePrice
 from api.admin.stripe_product import StripeProductAdmin
 from api.admin.stripe_subscription import StripeSubscriptionAdmin
+from api.admin.workspace import WorkspaceAdmin
