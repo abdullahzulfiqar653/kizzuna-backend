4e57c1d88774c1110805be812d4637a110c38463
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-08-13 06:46:21 +0500

    creation of clip and thumbnail on create and update as well as urls handled

--- api/serializers/playbook_takeaway.py
+++ api/serializers/playbook_takeaway.py
@@ -1,14 +1,31 @@
-from api.serializers.takeaway import TakeawaySerializer
+from django.conf import settings
+from rest_framework import serializers, exceptions
+
+from api.utils import media
 from api.models.takeaway import Takeaway
 from api.models.playbook import PlayBookTakeaway
-from rest_framework import serializers, exceptions
-from django.conf import settings
-from api.utils.media import merge_media_files
+from api.serializers.takeaway import TakeawaySerializer
+
+
+def create_playbook_clip_and_thumbnail(playbook):
+    playbook_takeaways = PlayBookTakeaway.objects.filter(
+        takeaway__in=playbook.takeaways.all()
+    ).order_by("-order")
+    urls = "\n".join(
+        f"file '{pt.takeaway.highlight.clip.url if settings.USE_S3 else pt.takeaway.highlight.clip.path}'"
+        for pt in playbook_takeaways
+    )
+    clip = media.merge_media_files(urls)
+    playbook.clip = clip
+    playbook.save()
+    playbook.thumbnail = media.create_thumbnail(playbook.clip, 1)
+    playbook.save()
 
 
 class PlaybookTakeawaySerializer(TakeawaySerializer):
-    order = serializers.IntegerField(required=False)
     clip = serializers.SerializerMethodField()
+    thumbnail = serializers.SerializerMethodField()
+    order = serializers.IntegerField(required=False)
     takeaway_id = serializers.CharField(required=False)
     type_id = None
 
@@ -19,7 +36,11 @@ class PlaybookTakeawaySerializer(TakeawaySerializer):
 
     def get_clip(self, obj):
         playbook = self.context.get("request").playbook
-        return playbook.clip.url
+        return playbook.clip.url if playbook.clip else None
+
+    def get_thumbnail(self, obj):
+        playbook = self.context.get("request").playbook
+        return playbook.thumbnail.url if playbook.thumbnail else None
 
     def validate_takeaway_id(self, takeaway_id):
         request = self.context.get("request")
@@ -43,23 +64,16 @@ class PlaybookTakeawaySerializer(TakeawaySerializer):
 
     def create(self, validated_data):
         request = self.context.get("request")
-        return PlayBookTakeaway.objects.create(
+        playbook_takeaway = PlayBookTakeaway.objects.create(
             playbook=request.playbook, takeaway_id=validated_data["takeaway_id"]
         )
+        create_playbook_clip_and_thumbnail(request.playbook)
+        return playbook_takeaway
 
     def update(self, takeaway, validated_data):
         request = self.context.get("request")
         PlayBookTakeaway.objects.get(playbook=request.playbook, takeaway=takeaway).to(
             validated_data["order"]
         )
-        playbook_takeaways = PlayBookTakeaway.objects.filter(
-            takeaway__in=request.playbook.takeaways.all()
-        ).order_by("-order")
-
-        urls = "\n".join(
-            f"file '{pt.takeaway.highlight.clip.url if settings.USE_S3 else pt.takeaway.highlight.clip.path}'"
-            for pt in playbook_takeaways
-        )
-        request.playbook.clip = merge_media_files(urls)
-        request.playbook.save()
+        create_playbook_clip_and_thumbnail(request.playbook)
         return super().update(request.playbook, validated_data)
